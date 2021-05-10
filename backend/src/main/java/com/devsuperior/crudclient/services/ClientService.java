5168bf39e248a2164ea26594d46fa715f7da0cd1
Carlos Leuck <carlinhosrlz@gmail.com>
2021-05-09 22:10:43 -0300

    Exception handling

--- backend/src/main/java/com/devsuperior/crudclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/crudclient/services/ClientService.java
@@ -3,11 +3,13 @@ package com.devsuperior.crudclient.services;
 import com.devsuperior.crudclient.dto.ClientDTO;
 import com.devsuperior.crudclient.entities.Client;
 import com.devsuperior.crudclient.repositories.ClientRepository;
+import com.devsuperior.crudclient.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,8 +19,15 @@ public class ClientService {
     private ClientRepository repository;
 
     @Transactional(readOnly = true)
-    public List<ClientDTO> findAll(){
-       List<Client> list= repository.findAll();
-       return list.stream().map(ClientDTO::new).collect(Collectors.toList());
+    public List<ClientDTO> findAll() {
+        List<Client> list = repository.findAll();
+        return list.stream().map(ClientDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Optional<Client> obj = repository.findById(id);
+        Client entity = obj.orElseThrow( () -> new ResourceNotFoundException("Entity not found"));
+        return new ClientDTO(entity);
     }
 }
